eb177a94e6a520e347ca89d6b9e9dea5f1699dd8
maheshwaran1303 <maheshwaran130324@gmail.com>
2025-11-14 18:55:56 +0530

    Debug = False

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gk2d-b&lt=p-sayk+j^#qo6xh%_#i&1$==#7(0009*&lw3r7&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 import os
 
